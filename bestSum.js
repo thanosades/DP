5f65b5ff5d4e9c627a09e9acc6b31b7369d30dc7
Thanos Dimitriades <thanosades@gmail.com>
2023-03-01 18:46:43 +0200

    complete bestSum and fix loop bug

---
+++ bestSum.js
@@ -0,0 +1,29 @@
+const bestSum = (target, numbers, memo = {}) => {
+    if (memo[target]) return memo[target];
+    if (target === 0) return [];
+    if (target < 0) return null;
+    let shortestArr = null;
+
+    for (let num of numbers) {
+        const remainder = target - num;
+        const res = bestSum(remainder, numbers, memo);
+        if (res !== null) {
+            const combi = [ ...res, num ];
+            if (shortestArr === null || combi.length < shortestArr.length) {
+                shortestArr = combi;
+            }
+        }
+    }
+
+    memo[target] = shortestArr;
+    return shortestArr;
+};
+
+// m = target
+// n = numbers length
+// O(n^m * m) time
+// O(m^2) space (because we store an array on every call stack of worst case m)
+
+console.log(bestSum(7, [5, 3, 4, 7]));
+console.log(bestSum(8, [2, 3, 5]));
+console.log(bestSum(100, [1, 2, 5, 25]));
