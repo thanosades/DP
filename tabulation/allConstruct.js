a074ece92da08dd148299e4e36bf486b23af3081
Thanos Dimitriades <thanosades@gmail.com>
2023-03-03 22:30:21 +0200

    add all construct with tabulation

---
+++ tabulation/allConstruct.js
@@ -0,0 +1,23 @@
+const allConstruct = (target, wordBank) => {
+    const table = Array(target.length + 1)
+        .fill()
+        .map(() => []);
+    
+    table[0] = [[]];
+
+    for (let i = 0; i <= target.length; i++) {
+        for (let word of wordBank) {
+            if (target.slice(i, i + word.length) === word) {
+                const newCombinations = table[i].map(subArr => [...subArr, word]);
+                table[i + word.length].push(...newCombinations);
+            }
+        }
+    }
+
+    return table[target.length];
+};
+
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
+console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']));
+
+console.log(allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
